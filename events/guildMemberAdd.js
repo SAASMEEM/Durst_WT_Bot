99619aa60a491e5fcb6fab7496a2a46aab8404b5
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2022-11-22 17:55:01 +0100

    fix feedback

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const botconfig = require('../config.json');
 
 module.exports = {
 	name: "guildMemberAdd",
@@ -17,7 +18,7 @@ module.exports = {
 		});
 		member.send({embeds: [embed]}).catch (e =>{
 			const channel = member.guild.channels.cache.get(botconfig.uffzChannelId)
-			channel.send(`<@${user.id}> konnte keine Direktnacht empfangen!`)
+			channel.send(`<@${member.user.id}> konnte keine Direktnacht empfangen!`)
 			return
 		})
     },
