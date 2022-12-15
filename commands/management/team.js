3d2ed183da2ddf5262c936d12a8e6e20224136a7
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2022-12-15 16:38:32 +0100

    correct format

--- commands/management/team.js
+++ commands/management/team.js
@@ -7,21 +7,21 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("team")
         .setDescription("manage teams")
-            .addSubcommand(subcommand =>
-                subcommand
-                    .setName('add')
-                    .setDescription('Add a user to the squadron')
-                    .addIntegerOption(option =>
-                        option
-                            .setName('role')
-                            .setDescription('Choose the mannschafter-role the user should get added')
-                            .setRequired(true)
-                            .setChoices(
-                                { name: 'Mannschafter (Main)', value: 1 },
-                                { name: 'Mannschafter (zweit)', value: 2 }
-                            )
-                    )
-            ),
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('add')
+                .setDescription('Add a user to the squadron')
+                .addIntegerOption(option =>
+                    option
+                        .setName('role')
+                        .setDescription('Choose the mannschafter-role the user should get added')
+                        .setRequired(true)
+                        .setChoices(
+                            { name: 'Mannschafter (Main)', value: 1 },
+                            { name: 'Mannschafter (zweit)', value: 2 }
+                        )
+                )
+        ),
     async execute(interaction) {
         // check for required permission
         const check = await checkPerms(
