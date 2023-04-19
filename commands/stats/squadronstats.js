c7f2d21ccdf02642a73216a4b40f12bbcd98b2af
asssssssra <e.terhell@gmail.com>
2023-04-19 15:10:32 +0200

    adding working command squadronstats

--- commands/stats/squadronstats.js
+++ commands/stats/squadronstats.js
@@ -1,6 +1,8 @@
-const Discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { DOMParser } = require('xmldom');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fetch = require('node-fetch');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,32 +11,55 @@ module.exports = {
         .addStringOption((option) =>
             option.setName("url").setDescription("url of the squad")
         ),
-    async execute(client, interaction) {
+    async execute(interaction) {
         const url = interaction.options.getString("url")
-        await interaction.reply(getstatact(url) + " " + getstatcount(url));
+        respond = "";
+        if (isValidUrl(url)) {
+          respond = "Die Kampgruppenaktivität ist aktuell " +await getstatact(url) + "\nDie Anzahl der Mitglieder ist: " + await getstatcount(url);
+          
+          
+          
+        } else {
+          respond ="Die URL ist ungültig!";
+        }
+        
+        await interaction.reply(respond);
     },
 };
 
+
+function isValidUrl(url) {
+  const regex = "https://warthunder.com/de/community/claninfo/";
+  return url.startsWith(regex);
+}
+
+
 async function getstatact(url) {
-    const response = await fetch(url);
-    const html = await response.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const link = doc.querySelector("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(3) > div > section > div.squadrons-profileheader-wrapper > div.squadrons-profileheader-aside.squadrons-counter.js-change-tabs > div.squadrons-countercount-wrapper > div:nth-child(2) > div.squadrons-countervalue");
-    const iact = parseInt(link.textContent.trim());
-    return iact;
+  const html = await geturldoc(url);
+  const dom = new JSDOM(html);
+  const element = dom.window.document.querySelector("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(3) > div > section > div.squadrons-profile__header-wrapper > div.squadrons-profile__header-aside.squadrons-counter.js-change-tabs > div.squadrons-counter__count-wrapper > div:nth-child(2) > div.squadrons-counter__value");
+  const iact = parseInt(element.textContent.trim());
+  return iact;
 }
 
 async function getstatcount(url) {
-    const response = await fetch(url);
-    const html = await response.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const count1 = doc.querySelector("#squadronsInfoRoot > div.squadrons-info__content-wrapper > div:nth-child(2)");
-    let temp = count1.textContent;
-    temp = temp.replace(/[A-z]/g, "");
-    temp = temp.replace(/ /g, "");
-    temp = temp.replace(/:/g, "");
-    const icount1 = parseInt(temp);
-    return icount1;
+  try {
+    const html = await geturldoc(url);
+    const doc = new JSDOM(html);
+     const count1 = doc.window.document.querySelector("#squadronsInfoRoot > div.squadrons-info__content-wrapper > div:nth-child(2)");
+     let temp = count1.textContent;
+     temp = temp.replace(/[A-z]/g, "");
+     temp = temp.replace(/ /g, "");
+     temp = temp.replace(/:/g, "");
+     const icount1 = parseInt(temp);
+     return icount1;
+  } catch (error) {
+     console.error('Error:', error);
+  }
+}
+
+async function geturldoc(url) {
+  const response = await fetch(url);
+  const html = await response.text();
+  return html;
 }
