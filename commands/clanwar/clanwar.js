07ed4233fc6c5bb248c9531e29f9ed8659e42ac3
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2023-06-12 09:56:11 +0200

    refactor: move command in filestructure and migrate to v14

--- commands/clanwar/clanwar.js
+++ commands/clanwar/clanwar.js
@@ -1,4 +1,4 @@
-import { MessageEmbed, MessageActionRow, MessageButton } from "discord.js";
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 
 export const data = new SlashCommandBuilder()
@@ -7,7 +7,7 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(client, interaction) {
     // declare embed
-    const tableEmbed = new MessageEmbed({
+    const tableEmbed = new EmbedBuilder({
         color: "880099",
         title: `Clanwar`,
         description: `⏲️ <t:1703372400:R>`,
@@ -19,37 +19,37 @@ export async function execute(client, interaction) {
         timestamp: Date.now(),
     });
     // declare buttons
-    const ReactionsRow1 = new MessageActionRow().addComponents(
-        new MessageButton()
+    const ReactionsRow1 = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
             .setEmoji("✅")
             .setCustomId("join")
-            .setStyle("SUCCESS"),
-        new MessageButton()
+            .setStyle("Success"),
+        new ButtonBuilder()
             .setEmoji("⛔")
             .setCustomId("leave")
-            .setStyle("DANGER"),
-        new MessageButton()
+            .setStyle("Danger"),
+        new ButtonBuilder()
             .setEmoji("❔")
             .setCustomId("maybe")
-            .setStyle("SECONDARY"),
-        new MessageButton()
+            .setStyle("Secondary"),
+        new ButtonBuilder()
             .setEmoji("⚙️")
             .setCustomId("setting")
-            .setStyle("PRIMARY")
+            .setStyle("Primary")
     );
-    const ReactionsRow2 = new MessageActionRow().addComponents(
-        new MessageButton()
+    const ReactionsRow2 = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
             .setLabel("Checkliste")
             .setURL(
                 "https://docs.google.com/document/d/122R5Y0781w2aP26meX7Q0kLAGb_vvE_0jUju_FUm0W8"
             )
-            .setStyle("LINK"),
-        new MessageButton()
+            .setStyle("Link"),
+        new ButtonBuilder()
             .setLabel("Fahrzeugaufstellung")
             .setURL(
                 "https://docs.google.com/spreadsheets/d/1bfej9szrHsddB29cabYDo1YZjqJ3amNAwhvNfDJzlAY"
             )
-            .setStyle("LINK")
+            .setStyle("Link")
     );
     await interaction.channel.send({
         embeds: [tableEmbed],
